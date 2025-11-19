f5e438eaf6c25d689b2f07480b43fac751520616
Delba de Oliveira <32464864+delbaoliveira@users.noreply.github.com>
2025-11-19 14:49:45 +0000

    Upgrade tooling, add Next.js MCP (#90)
    * Upgrade to next.js 16
    
    * Add eslint
    
    * Add prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
-
 export default function Home() {
   return (
     <div className="h-screen w-screen flex items-center justify-center">
       Cooking.
     </div>
-  );
+  )
 }
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,19 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import prettier from 'eslint-config-prettier/flat'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  prettier,
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+    // Content Collections generated files:
+    '.content-collections/**',
+  ]),
+])
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default eslintConfig
